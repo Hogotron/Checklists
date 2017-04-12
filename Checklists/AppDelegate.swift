91b5c896d100b38e0449afce94156d7684f6cfdb
Hogotron <trsidenfaden@yahoo.com>
2017-04-12 13:44:26 -0700

    Added user notifications to halfway through page 238

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
@@ -21,10 +22,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let controller = navigationController.viewControllers[0]
                                             as! AllListsViewController
         controller.dataModel = dataModel
-        
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
         return true
     }
 
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("Received local user notification \(notification)")
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
